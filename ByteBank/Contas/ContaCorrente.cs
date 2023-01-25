2c7bbd42b62fd416e37338bb4202eb28ad58dc7c
adrieliket <adrielikethin.dossantos@gmail.com>
2023-01-25 16:34:31 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank/Contas/ContaCorrente.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Titular;
+
+namespace ByteBank.Contas
+{
+    public class ContaCorrente
+    {
+        public static int TotalDeContasCriadas { get; private set; }
+        private int numeroAgencia;
+        public int NumeroAgencia
+        {
+            get { return numeroAgencia; }
+            private set 
+            { 
+                if (value >0)
+                {
+                    numeroAgencia = value;
+                }
+            }
+
+        }
+        public string Conta { get; private set; }
+        public double Saldo { get; set; }
+        public Cliente Titular { get; set; }
+
+        public ContaCorrente(int numeroAgencia, string conta, Cliente titular)
+        {
+            this.numeroAgencia=numeroAgencia;
+            Conta = conta;
+            Titular = titular;
+            TotalDeContasCriadas++;
+        }
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor <= Saldo)
+            {
+                Saldo -= valor;
+                Console.WriteLine("Saldo em conta pós saque: " + Saldo);
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente para saque");
+            }
+        }
+
+        public bool Tranferir(double valor, ContaCorrente destino)
+        {
+            if (Saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                Saldo -= valor;
+                destino.Depositar(valor);
+                return true;
+            }
+        }
+
+        public void ImprimirDados()
+        {
+            Console.WriteLine("Número da agência: " + numeroAgencia);
+            Console.WriteLine("Conta: " + Conta);
+            Console.WriteLine("Titular: " + Titular.Nome);
+            Console.WriteLine("Saldo: " + Saldo);
+        }
+    }
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,40 @@
+﻿using ByteBank.Contas;
+using ByteBank.Titular;
+
+Cliente cliente1= new Cliente("Adrieli Kethin dos Santos", "10000000000");
+cliente1.Profissao = "Desenvolvedor de sistemas";
+
+ContaCorrente conta1 = new ContaCorrente(15, "1010-X", cliente1);
+conta1.Saldo = 100;
+
+ContaCorrente conta2 = new ContaCorrente(17, "1010-5", new Cliente("Jean Oliveira", "00000000000"));
+conta2.Titular.Profissao = "Promotor de vendas";
+conta2.Saldo = 350;
+
+Console.WriteLine("Saldo em conta: " + conta1.Saldo);
+
+//Deposito
+conta1.Depositar(100);
+Console.WriteLine("Saldo em conta pós deposito: " + conta1.Saldo);
+Console.WriteLine();
+
+//Saque
+conta1.Sacar(300);
+Console.WriteLine();
+
+Console.WriteLine("Saldo pessoa 1: " + conta1.Saldo);
+Console.WriteLine("Saldo pessoa 2: " + conta2.Saldo);
+Console.WriteLine();
+
+//Transferência
+conta1.Tranferir(50, conta2);
+Console.WriteLine("Saldo pessoa 1 após transferência: " + conta1.Saldo);
+Console.WriteLine("Saldo pessoa 2 após transferência: : " + conta2.Saldo);
+Console.WriteLine();
+
+conta1.ImprimirDados();
+Console.WriteLine();
+conta2.ImprimirDados();
+
+//Propriedade estática da classe
+Console.WriteLine("Total de contas criadas: " + ContaCorrente.TotalDeContasCriadas);
